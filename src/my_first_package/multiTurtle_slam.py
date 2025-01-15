f73c7d2c7120902938c7bcbf48bdea6684978d93
RishiAc <rishiac@outlook.com>
2025-01-15 12:12:37 -0500

    fixed nav issue with second bot

--- src/my_first_package/multiTurtle_slam.py
+++ src/my_first_package/multiTurtle_slam.py
@@ -9,7 +9,7 @@ import tf
 from math import radians, copysign, sqrt, pow, pi, atan2
 from tf.transformations import euler_from_quaternion
 import numpy as np
-
+import os
 
 
 uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
@@ -29,22 +29,22 @@ def launch_all_robots(x):
 def launch_all_nav(x):
     
     for i in range(x):
-            if i == 0:
-                launch_file = "src/turtlebot3/turtlebot3_navigation/launch/turtlebot3_navigation.launch"
-            # Launch arguments should be passed as a list of strings, not tuples
-                launch_args = ['first_tb3:=tb3_' + str(i)]
+        launch_file = "src/turtlebot3/turtlebot3_navigation/launch/turtlebot3_navigation.launch"
+        # Launch arguments should be passed as a list of strings, not tuples
+        launch_args = ['first_tb3:=tb3_' + str(i),"open_rviz:=false"]
 
-                launch = roslaunch.parent.ROSLaunchParent(uuid, [(launch_file, launch_args)])
-                launch.start()
-            else:
-                launch_file = "src/turtlebot3/turtlebot3_navigation/launch/turtlebot3_navigation.launch"
-            # Launch arguments should be passed as a list of strings, not tuples
-                launch_args = ['first_tb3:=tb3_' + str(i), "open_rviz:=false"]
+        launch = roslaunch.parent.ROSLaunchParent(uuid, [(launch_file, launch_args)])
+        launch.start()
+                 
+        '''else:
+            launch_file = "src/turtlebot3/turtlebot3_navigation/launch/turtlebot3_navigation.launch"
+        # Launch arguments should be passed as a list of strings, not tuples
+            launch_args = ['first_tb3:=tb3_' + str(i), "open_rviz:=false"]
 
-                launch = roslaunch.parent.ROSLaunchParent(uuid, [(launch_file, launch_args)])
-                launch.start()
-            rospy.loginfo(f"TurtleBot tb3_{i} launched")
-            time.sleep(2)  # Add a delay to ensure proper initialization
+            launch = roslaunch.parent.ROSLaunchParent(uuid, [(launch_file, launch_args)])
+            launch.start()'''
+        rospy.loginfo(f"TurtleBot tb3_{i} launched")
+        time.sleep(2)  # Add a delay to ensure proper initialization
 
 def launch_all_slam(x):
     
@@ -70,6 +70,40 @@ def launch_all_map(x):
             rospy.loginfo(f"TurtleBot tb3_{i} launched")
             time.sleep(2)  # Add a delay to ensure proper initialization
 
+def launch_rviz():
+    launch_file = "src/turtlebot3/turtlebot3_navigation/launch/launch_rviz.launch"
+    launch = roslaunch.parent.ROSLaunchParent(uuid, [(launch_file)])
+    launch.start()
+
+def update_rviz_config(robot_number):
+    # Path to your RViz configuration file
+    rviz_config_path = os.path.expanduser('src/turtlebot3/turtlebot3_navigation/rviz/turtlebot3_navigation1.rviz')
+    
+    # Define the new topic based on the robot number
+    new_topic = f'/tb3_{robot_number}/move_base_simple/goal'
+    
+    # Read the existing RViz config
+    with open(rviz_config_path, 'r') as file:
+        rviz_config = file.readlines()
+    
+    # Update the 2D Nav Goal topic in the config
+    with open(rviz_config_path, 'w') as file:
+        for line in rviz_config:
+            # Look for the "Topic" line under "SetGoal" and update it
+            if 'Name: Goal' in line:
+                # After finding the SetGoal tool, update the next "Topic" entry
+                next_line_index = rviz_config.index(line) + 1
+                if 'Topic' in rviz_config[next_line_index]:
+                    rviz_config[next_line_index] = f'    Topic: {new_topic}\n'
+            file.write(line)
+            if '- Class: rviz/SetGoal' in line:
+                # After finding the SetGoal tool, update the next "Topic" entry
+                next_line_index = rviz_config.index(line) + 1
+                if 'Topic' in rviz_config[next_line_index]:
+                    rviz_config[next_line_index] = f'    Topic: {new_topic}\n'
+            file.write(line)
+
+    print(f'Updated RViz config for robot {robot_number} with 2D Nav Goal topic: {new_topic}')
 
 if __name__ == '__main__':
     rospy.init_node("multi_robot_thing")
@@ -89,7 +123,7 @@ if __name__ == '__main__':
     launch_all_map(x)
 
     launch_all_nav(x)
-
+    launch_rviz()
     done = "False"
 
     while done != "True":
